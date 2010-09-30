8714dd732f121e03cee7109be7bd132c3a847784
Andre Pankratz <andre@webwarelab.com>
2010-09-30 15:55:40 +0200

    Added view helper #number_to_duration

--- lib/vidibus/helpers/extensions/view.rb
+++ lib/vidibus/helpers/extensions/view.rb
@@ -16,6 +16,30 @@ module Vidibus
           @autotab ||= 0
           @autotab += 1
         end
+
+        # Formats a number or float as human duration.
+        #
+        # Examples:
+        #
+        #   number_to_duration(24.5)                  # => "00:25"
+        #   number_to_duration(24.5, :hours => true)  # => "00:00:25"
+        #   number_to_duration(3654)                  # => "01:00:54"
+        #   number_to_duration(3654, :hours => false) # => "60:54"
+        #
+        def number_to_duration(seconds, options = {})
+          hours = options[:hours]
+          seconds = seconds.round
+          hour = (seconds / 3600).floor unless hours == false
+          min  = (seconds / 60).floor
+          sec  = (seconds % 60).round
+          if hours != false and (hours == true or hour > 0)
+            min -= hour * 60
+            output = "%02d:" % hour
+          else
+            output = ''
+          end
+          output + "%02d:%02d" % [ min, sec ]
+        end
       end
     end
   end
---
+++ spec/vidibus/helpers/extensions/view_spec.rb
@@ -0,0 +1,29 @@
+require "spec_helper"
+
+class ViewHelper
+  include Vidibus::Helpers::Extensions::View
+end
+
+describe ViewHelper do
+  describe "#number_to_duration" do
+    it "should return 00:24 for 24 seconds" do
+      subject.number_to_duration(24).should eql("00:24")
+    end
+
+    it "should return 00:00:24 for 24 seconds if :hours option is true" do
+      subject.number_to_duration(24, :hours => true).should eql("00:00:24")
+    end
+
+    it "should return 01:00:54 for 3654 seconds" do
+      subject.number_to_duration(3654, :hours => true).should eql("01:00:54")
+    end
+
+    it "should return 01:00:55 for 3654.5 seconds" do
+      subject.number_to_duration(3654.5, :hours => true).should eql("01:00:55")
+    end
+
+    it "should return 60:54 for 3654 seconds if :hours option is false" do
+      subject.number_to_duration(3654, :hours => false).should eql("60:54")
+    end
+  end
+end
